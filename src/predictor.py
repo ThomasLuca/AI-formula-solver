add608ba1bfc37ada5cd777a13d8640af4f94706
DevNode <thomasluca008@gmail.com>
2021-04-25 13:20:31 +0200

    fixed indentation

--- src/predictor.py
+++ src/predictor.py
@@ -9,12 +9,12 @@ class Predictor:
         self.model = load_model('../digit_recognizer.h5')
     
     def format_image(self, img):
-            img = cv2.resize(img, (28, 28), interpolation = cv2.INTER_AREA)
-            img = np.array(img)
-            img = img.astype(np.float32).reshape(-1, 784)
-            img = 255 - img
-            img /= 255
-            return img
+        img = cv2.resize(img, (28, 28), interpolation = cv2.INTER_AREA)
+        img = np.array(img)
+        img = img.astype(np.float32).reshape(-1, 784)
+        img = 255 - img
+        img /= 255
+        return img
     
     def predict(self, img):
         formatted = self.format_image(img)
